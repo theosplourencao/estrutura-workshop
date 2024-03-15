e2a951e8fe2f4c219b0f0421c21e42a9c9530a50
6181-NB\theo.lourencao_ifood <theo.lourencao@ifood.com>
2024-03-15 20:18:17 -0300

    sera que vai

--- app/main.py
+++ app/main.py
@@ -1,5 +1,12 @@
-from pipeline import extract
+from pipeline.extract import extract_from_excel
+from pipeline.transform import contact_data_frames
+from pipeline.load import load_excel
 
-listas_de_data_frame=extract.extract_from_excel("data/input")
 
-print(listas_de_data_frame)
+if __name__=="__main__":
+    data_frame_list = extract_from_excel("data/input")
+    print(type(data_frame_list))
+    data_frame= contact_data_frames(data_frame_list)
+    print(type(data_frame))
+    load_excel(data_frame, "data/output", "output")
+    
--- app/pipeline/__init__.py
+++ app/pipeline/__init__.py
@@ -1,5 +1,6 @@
 """This module contains functions for the ETL process."""
 
-from app.pipeline.extract import extract_from_excel
+from .extract import extract_from_excel
 
-from app.pipeline.transform import contact_data_frames
+from .transform import contact_data_frames
+from .load import load_excel
