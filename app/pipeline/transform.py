1ac9dc91d6de681abbcb030d0da258ae9f3fb4a3
6181-NB\theo.lourencao_ifood <theo.lourencao@ifood.com>
2024-03-15 22:12:13 -0300

    agora vai essa biceti

--- app/pipeline/transform.py
+++ app/pipeline/transform.py
@@ -2,4 +2,14 @@ import pandas as pd
 from typing import List
 
 def contact_data_frames(data_frame_list: List[pd.DataFrame]) -> pd.DataFrame:
+    """
+    Transform your dataframe list into one unique dataframe
+    _summary_
+
+    Args:
+        data_frame_list (List[pd.DataFrame]): _description_
+
+    Returns:
+        pd.DataFrame: _description_
+    """
     return pd.concat(data_frame_list, ignore_index=True)
